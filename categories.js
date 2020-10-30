4e813162e12f46fe03a14ec48f8d41cfc15f1172
justlawly <44566988+justlawly@users.noreply.github.com>
2020-10-30 19:13:37 +0200

    Add files via upload

---
+++ categories.js
@@ -0,0 +1,7 @@
+export default categoryList = [
+    "All",
+    "Dogs",
+    "Cats",
+    "Budha",
+    "Soap", 
+];
---
+++ gameData.js
@@ -0,0 +1,72 @@
+export default games = [
+    {
+        id: 1,
+        title: "Budha",
+        
+        description:
+            "egolden budha",
+        rating: 4.5,
+        category: ["All",, "Budha"],
+        
+        cover: require("./assets/Budha_1.jpg"),
+        screenshots: [
+            require("./assets/Budha_2.jpg"),
+           
+        ],
+        backgroundColor: "#824571",
+    },
+    {
+        id: 2,
+        title: "Cats",
+        
+        description:
+            " חתול",
+        rating: 3.8,
+        category: ["All","Cats"],
+        
+        cover: require("./assets/ori_1.jpg"),
+        screenshots: [
+            require("./assets/ori_2.jpg"),
+            
+        ],
+        backgroundColor: "#456382",
+        
+    },
+    {
+        
+        id: 3,
+        title: "Dog",
+        
+        description:
+            " dogs",
+        rating: 2.4,
+        category: ["All","Dogs",],
+        
+        cover: require("./assets/dog_1.jpg"),
+        screenshots: [
+            require("./assets/dog_2.jpg"),
+            
+        ],
+        backgroundColor: "#825D45",
+        
+    },
+
+    {
+        
+        id: 4,
+        title: "Soap",
+        
+        description:
+            " soaps",
+        rating: 2.4,
+        category: ["All","Soap",],
+        
+        cover: require("./assets/Soap_1.jpg"),
+        screenshots: [
+            require("./assets/Soap_2.jpg"),
+            
+        ],
+        backgroundColor: "#825D45",
+        
+    },
+];
